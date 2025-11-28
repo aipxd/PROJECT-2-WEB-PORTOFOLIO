f920c484ce949608e671cb3d2c575053a61d936c
AIP-Code <aiprojectxd@gmail.com>
2025-11-28 23:25:22 +0700

    #3 Final Project

--- porto.js
+++ porto.js
@@ -1,3 +1,4 @@
+// Navigation Active Link
 const links = document.querySelectorAll('.nav-link');
 
 links.forEach(link => {
@@ -8,3 +9,57 @@ links.forEach(link => {
   );
 }
 );
+
+
+// Typewriter Effect
+const firstText = "Hi, It's ";
+const nameText = "Adibta";
+let index1 = 0;
+let index2 = 0;
+
+const typeEl = document.getElementById("typewriter");
+const nameEl = document.getElementById("name");
+
+function typeFirst() {
+  if (index1 < firstText.length) {
+    typeEl.textContent += firstText[index1];
+    index1++;
+    setTimeout(typeFirst, 90);
+  } 
+  else {
+    typeEl.classList.remove("cursor");
+    typeName();
+  }
+}
+
+function typeName() {
+  if (index2 < nameText.length) {
+    nameEl.textContent += nameText[index2];
+    index2++;
+    setTimeout(typeName, 90);
+  }
+}
+
+typeFirst();
+
+
+//Fade In Effect
+const fadeElements = document.querySelectorAll(".fade-in");
+
+function checkFadeIn() {
+  fadeElements.forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkFadeIn);
+checkFadeIn(); 
+
+
+//Cursor Effect
+document.addEventListener("mousemove", (e) => {
+  const glow = document.querySelector(".hero::before");
+});
