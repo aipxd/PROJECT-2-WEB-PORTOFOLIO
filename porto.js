199bc0d4c1aea1eb832e721153c8f5710f7ddb78
AIP-Code <aiprojectxd@gmail.com>
2025-11-28 10:55:42 +0700

    #2 WEB PORTOFOLIO PROJECT

--- porto.js
+++ porto.js
@@ -1,11 +1,10 @@
 const links = document.querySelectorAll('.nav-link');
 
 links.forEach(link => {
-  link.addEventListener('click', function() 
-  {
+  link.addEventListener('click', function () {
     links.forEach(item => item.classList.remove('active'));
     this.classList.add('active');
   }
-);
+  );
 }
 );
